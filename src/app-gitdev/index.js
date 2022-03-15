989f0d0ed69a4117f86b1e742ecc9d332d8505c5
Hardik Gohil <32103022+HardikGohilHLR@users.noreply.github.com>
2022-03-15 22:46:08 +0530

    initial commit

---
+++ src/app-gitdev/index.js
@@ -0,0 +1,19 @@
+/* Main Component */ 
+
+import React from 'react';
+
+const GitDev = () => {
+
+    return (
+        <React.Fragment>
+            <div className="content">
+                <div className="app-container">
+
+                    <h1>GitDev Finder</h1>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default GitDev;
